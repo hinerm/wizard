4c5d95f880d5b1513385fc49d5eb37648ed7fd6e
Mark Hiner <hinerm@gmail.com>
2015-02-12 10:58:00 -0600

    Add wizard source

---
+++ src/main/java/org/scijava/ui/swing/wizard/WizardPanelNotFoundException.java
@@ -0,0 +1,14 @@
+
+package org.scijava.ui.swing.wizard;
+
+/**
+ * Adapted Robert Eckstein's sample at
+ * http://java.sun.com/developer/technicalArticles/GUI/swing/wizard/
+ */
+public class WizardPanelNotFoundException extends RuntimeException {
+
+	public WizardPanelNotFoundException() {
+		super();
+	}
+
+}
